f8b211219d89a9b9c1bb382188005fef6e18aa20
Dave Stevens <dave@crowdlab.com>
2015-04-12 12:26:12 +0100

    Int array test.

---
+++ test/src/int_array_test.c
@@ -0,0 +1,26 @@
+#include "test.h"
+#include "list.h"
+#include <stdlib.h>
+
+TestStatus int_array_test(void)
+{
+  List *list = list_new();
+  uint32_t integer_1 = 123, integer_2 = 456, integer_3 = 789;
+  list->push(list, INT, (void *)&integer_1);
+  list->push(list, INT, (void *)&integer_2);
+  list->push(list, INT, (void *)&integer_3);
+  printf("Converting to an Integer array");
+
+  uint32_t *array = list->int_array(list);
+
+  {
+    uint32_t size = list->count;
+    uint32_t expected_array[] = { integer_1, integer_2, integer_3 };
+    uint32_t index;
+    for(index = 0; index < size; ++index) {
+      if (array[index] != expected_array[index]) { return FAIL; }
+    }
+  }
+
+  return PASS;
+}
